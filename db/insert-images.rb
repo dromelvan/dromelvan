7e15363a781e96b6c845ba73553d60b323cd9850
dromelvan <dromelvan@aland.net>
2018-07-13 13:28:06 +0300

    Player photos.

--- db/insert-images.rb
+++ db/insert-images.rb
@@ -1,49 +1,52 @@
 require 'fileutils'
 # Run this with: rails runner "eval(File.read 'db/insert-images.rb')"
 
-Dir.entries("project/graphics/player-photos").each do |file_name|
+Dir.entries("project/graphics/insert").each do |file_name|
   found = false
-  name = file_name.sub('.jpg','').parameterize
-  player = Player.where(parameterized_name: name).first
-  if !player.nil? #&& player.player_photo_file_name.nil?
+  name = file_name.sub('.png','').parameterize
+  players = Player.where(parameterized_name: name)
+  if players.size == 1
+    player = players.first  
     found = true
-    player.player_photo = File.new("project/graphics/player-photos/#{file_name}")
+    player.player_photo = File.new("project/graphics/insert/#{file_name}")
     player.save
-  else
+  end
+  
+  if !found
     players = Player.named(name)
     if players.size == 1
       player = players.first
-      #if player.player_photo_file_name.nil?
-        found = true
-        player.player_photo = File.new("project/graphics/player-photos/#{file_name}")
-        player.save              
-      #end
+      found = true
+      player.player_photo = File.new("project/graphics/insert/#{file_name}")
+      player.save              
     end
   end
   
   if !found
     puts("Did not find #{file_name}")
   else
-    FileUtils.mv("project/graphics/player-photos/#{file_name}", "project/graphics/moved/#{file_name}")
+    FileUtils.mv("project/graphics/insert/#{file_name}", "project/graphics/moved/#{file_name}")
   end
 end
 
-Dir.entries("project/graphics/club-crests").each do |file_name|
+=begin
+Dir.entries("project/graphics/insert").each do |file_name|
   code = file_name.sub('.png','')
   team = Team.where(code: code).first
   if !team.nil?
-    team.club_crest = File.new("project/graphics/club-crests/#{file_name}")
+    team.club_crest = File.new("project/graphics/insert/#{file_name}")
     team.save
   end  
 end
 
-Dir.entries("project/graphics/stadium-photos").each do |file_name|
+Dir.entries("project/graphics/insert").each do |file_name|
   name = file_name.sub('.png','')
   stadium = Stadium.where(name: name).first
   if !stadium.nil?
-    stadium.photo = File.new("project/graphics/stadium-photos/#{file_name}")
+    stadium.photo = File.new("project/graphics/insert/#{file_name}")
     stadium.save
   else
     puts("Did not find stadium #{name}.")
   end  
 end
+=end
