5808cb14624b0fc27fb6aa4e27ec9aa36ad123a9
dromelvan <dromelvan@aland.net>
2018-09-05 08:30:42 +0300

    Removed unused schema change.

--- db/schema.rb
+++ db/schema.rb
@@ -13,35 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20180817165956) do
 
-  create_table "api_users", force: true do |t|
-    t.string   "email"
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.string   "name"
-    t.string   "nickname"
-    t.string   "image"
-    t.string   "provider"
-    t.string   "uid",                    default: "", null: false
-    t.text     "tokens"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "api_users", ["email"], name: "index_api_users_on_email"
-  add_index "api_users", ["reset_password_token"], name: "index_api_users_on_reset_password_token", unique: true
-  add_index "api_users", ["uid"], name: "index_api_users_on_uid", unique: true
-
   create_table "cards", force: true do |t|
     t.integer  "match_id"
     t.integer  "team_id"
