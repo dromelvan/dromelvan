0388478cfaaf06eaf26fc5605e1fdb885e1f7826
dromelvan <dromelvan@aland.net>
2019-08-09 23:04:31 +0300

    Match day activate bugfix.

--- app/controllers/match_days_controller.rb
+++ app/controllers/match_days_controller.rb
@@ -1,7 +1,7 @@
 class MatchDaysController < ApplicationController
   include Select, StatusEnum
-  
-  # TODO: Figure out why specs fail if this is put in StatusEnum 
+
+  # TODO: Figure out why specs fail if this is put in StatusEnum
   before_action :authorize_administrator,  only: [:pend, :activate, :finish, :update, :update_stats]
 
   def current
@@ -17,21 +17,21 @@ class MatchDaysController < ApplicationController
       not_found
     end
   end
-  
+
   def pend
     match_day = MatchDay.find(status_enum_params[:id])
     update_status(match_day, :pending)
-    
+
     match_day.matches.each do |match|
       match.player_match_stats.delete_all
     end
   end
 
   def activate
+    match_day = MatchDay.find(status_enum_params[:id])
     if match_day.pending?
-      match_day = MatchDay.find(status_enum_params[:id])
       update_status(match_day, :active)
-      
+
       PlayerSeasonInfo.where(season: match_day.premier_league.season).each do |player_season_info|
         match = match_day.matches.by_team(player_season_info.team).take
         PlayerMatchStat.create(player: player_season_info.player, match: match, team: player_season_info.team, d11_team: player_season_info.d11_team, position: player_season_info.position, played_position: player_season_info.position.code)
@@ -45,22 +45,22 @@ class MatchDaysController < ApplicationController
     @match_day = MatchDay.find(params[:id])
     date_str = update_params[:date]
 
-    begin    
+    begin
       if !date_str.empty?
         @match_day.date = Date.parse(date_str)
-      else 
+      else
         @match_day.date = @match_day.date.postpone
       end
-      
+
       if @match_day.save
         flash[:success] = "Match day date updated."
       else
         flash[:danger] = "The match day date could not be updated."
-      end      
+      end
     rescue ArgumentError
       flash[:danger] = "The date '#{date_str}' is invalid."
     end
-    
+
     redirect_to @match_day
   end
 
@@ -71,22 +71,22 @@ class MatchDaysController < ApplicationController
       TeamTableStat.update_stats_from(match)
     end
     TeamTableStat.update_rankings_from(@match_day)
-    
+
     @match_day.d11_match_day.d11_matches.each do |d11_match|
       if d11_match.finished?
         D11TeamTableStat.update_stats_from(d11_match)
-      end      
+      end
     end
     D11TeamTableStat.update_rankings_from(@match_day.d11_match_day)
 
     flash[:success] = "Table statistics updated."
-    
+
     redirect_to @match_day
   end
-  
+
   private
     def update_params
       params.require(:match_day).permit(:date)
     end
-    
+
 end
